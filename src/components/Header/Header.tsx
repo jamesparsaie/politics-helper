f55be238a24fe523b8653292fb9edbef097af583
James <parsaiejames@gmail.com>
2025-07-13 21:35:05 -0500

    Inital setup of navbar and home page, white space issue needs investigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
 import { Burger, Container, Group } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { Link } from '@tanstack/react-router'
+import { FaFlagUsa } from "react-icons/fa";
 import classes from './Header.module.css';
 
 const links = [
@@ -13,22 +13,6 @@ const links = [
 
 export function Header() {
   const [opened, { toggle }] = useDisclosure(false);
-  const [active, setActive] = useState(links[0].link);
-
-//   const items = links.map((link) => (
-//     <a
-//       key={link.label}
-//       href={link.link}
-//       className={classes.link}
-//       data-active={active === link.link || undefined}
-//       onClick={(event) => {
-//         event.preventDefault();
-//         setActive(link.link);
-//       }}
-//     >
-//       {link.label}
-//     </a>
-//   ));
 
     const items = links.map((link) => (
         <Link  to={link.link} className={classes.link}>
@@ -39,11 +23,15 @@ export function Header() {
   return (
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
+        <Group>
+            <FaFlagUsa size={24}/>
+            <h2>Politrack</h2>
+        </Group>
         <Group gap={5} visibleFrom="xs">
           {items}
         </Group>
 
-        <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
+        <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" color='white'/>
       </Container>
     </header>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import classes from "../PageCSS/home.module.css"
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -6,7 +7,7 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return (
-    <div className="p-2">
+    <div className={classes.mainContent}>
       <h3>Welcome Home!</h3>
     </div>
   )
